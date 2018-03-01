4334954e09a1edc74cf59eb514b2a55bf47e1c91
Pravin Raj <pravin.raj@coveiot.com>
2018-03-01 10:40:30 +0530

    Testing RAGA

--- noble_examples/peripheral-explorer.js
+++ noble_examples/peripheral-explorer.js
@@ -1,16 +1,9 @@
 var async = require('async');
-var noble = require('noble');
- var Gpio = require('onoff').Gpio,
-	tv = new Gpio(17,'out'),
-	ac = new Gpio(27, 'out'),
-	light = new Gpio(22, 'out');
-var isFirstNotificationAfterConnect = false;
+var noble = require('../index');
 
-var peripheralIdOrAddress = 'RAGA_0209';
+var peripheralIdOrAddress = 'd2842a5ba5b8';
 
-console.log('  peripheralIdOrAddress        = ' + peripheralIdOrAddress);
-
-noble.on('stateChange', function(state) {
+noble.on('stateChange', function (state) {
   if (state === 'poweredOn') {
     noble.startScanning();
   } else {
@@ -18,8 +11,8 @@ noble.on('stateChange', function(state) {
   }
 });
 
-noble.on('discover', function(peripheral) {
-  if (peripheral.advertisement.localName === peripheralIdOrAddress) {
+noble.on('discover', function (peripheral) {
+  if (peripheral.id === peripheralIdOrAddress || peripheral.address === peripheralIdOrAddress) {
     noble.stopScanning();
 
     console.log('peripheral with ID ' + peripheral.id + ' found');
@@ -60,20 +53,19 @@ noble.on('discover', function(peripheral) {
 function explore(peripheral) {
   console.log('services and characteristics:');
 
-  peripheral.on('disconnect', function() {
+  peripheral.on('disconnect', function () {
     process.exit(0);
   });
 
-  peripheral.connect(function(error) {
-  isFirstNotificationAfterConnect = true;
-    peripheral.discoverServices(['000056ef00001000800000805f9b34fb'], function(error, services) {
+  peripheral.connect(function (error) {
+    peripheral.discoverServices([], function (error, services) {
       var serviceIndex = 0;
 
       async.whilst(
         function () {
           return (serviceIndex < services.length);
         },
-        function(callback) {
+        function (callback) {
           var service = services[serviceIndex];
           var serviceInfo = service.uuid;
 
@@ -82,14 +74,14 @@ function explore(peripheral) {
           }
           console.log(serviceInfo);
 
-          service.discoverCharacteristics(['000034e200001000800000805f9b34fb'], function(error, characteristics) {
+          service.discoverCharacteristics([], function (error, characteristics) {
             var characteristicIndex = 0;
-	    var buttonPushNotification = characteristics[0];
+
             async.whilst(
               function () {
                 return (characteristicIndex < characteristics.length);
               },
-              function(callback) {
+              function (callback) {
                 var characteristic = characteristics[characteristicIndex];
                 var characteristicInfo = '  ' + characteristic.uuid;
 
@@ -98,20 +90,20 @@ function explore(peripheral) {
                 }
 
                 async.series([
-                  function(callback) {
-                    characteristic.discoverDescriptors(function(error, descriptors) {
+                  function (callback) {
+                    characteristic.discoverDescriptors(function (error, descriptors) {
                       async.detect(
                         descriptors,
-                        function(descriptor, callback) {
+                        function (descriptor, callback) {
                           if (descriptor.uuid === '2901') {
                             return callback(descriptor);
                           } else {
                             return callback();
                           }
                         },
-                        function(userDescriptionDescriptor){
+                        function (userDescriptionDescriptor) {
                           if (userDescriptionDescriptor) {
-                            userDescriptionDescriptor.readValue(function(error, data) {
+                            userDescriptionDescriptor.readValue(function (error, data) {
                               if (data) {
                                 characteristicInfo += ' (' + data.toString() + ')';
                               }
@@ -124,11 +116,11 @@ function explore(peripheral) {
                       );
                     });
                   },
-                  function(callback) {
-                        characteristicInfo += '\n    properties  ' + characteristic.properties.join(', ');
+                  function (callback) {
+                    characteristicInfo += '\n    properties  ' + characteristic.properties.join(', ');
 
                     if (characteristic.properties.indexOf('read') !== -1) {
-                      characteristic.read(function(error, data) {
+                      characteristic.read(function (error, data) {
                         if (data) {
                           var string = data.toString('ascii');
 
@@ -140,67 +132,14 @@ function explore(peripheral) {
                       callback();
                     }
                   },
-                  function() {
+                  function () {
                     console.log(characteristicInfo);
                     characteristicIndex++;
                     callback();
                   }
                 ]);
-		buttonPushNotification.on('data', function(data, isNotification) {
-			if(!isFirstNotificationAfterConnect) {
-			    console.log('Notification received ' + data);
-				if (data == 'S1') {
-					console.log('Toggling TV');
-					tv.read(function(err, value){
-						if (err) {
-							console.log('Error reading TV led value');
-							peripheral.disconnect();
-						}
-						tv.write(value ^ 1, function(err) {
-							if (err) {
-								console.log('Error writing TV led value');
-								peripheral.disconnect();
-							}
-						});
-					});
-				} else if (data == 'S2') {
-					console.log('Toggling Light');
-						light.read(function(err, value){
-						if (err) {
-							console.log('Error reading Light led value');
-							peripheral.disconnect();
-						}
-						light.write(value ^ 1, function(err) {
-							if (err) {
-								console.log('Error writing Light led value');
-								peripheral.disconnect();
-							}
-						});
-					});
-				} else if (data == 'S3') {
-					console.log('Toggling AC');
-						ac.read(function(err, value){
-						if (err) {
-							console.log('Error reading AC led value');
-							peripheral.disconnect();
-						}
-						ac.write(value ^ 1, function(err) {
-							if (err) {
-								console.log('Error writing AC led value');
-								peripheral.disconnect();
-							}
-						});
-					});
-				}
-			} else {
-				isFirstNotificationAfterConnect = false;
-			}
-		});
-		buttonPushNotification.subscribe(function(error){
-			console.log('Subscribed to button press');
-		});
               },
-              function(error) {
+              function (error) {
                 serviceIndex++;
                 callback();
               }
@@ -208,9 +147,7 @@ function explore(peripheral) {
           });
         },
         function (err) {
-          //peripheral.disconnect();
-	console.log('Finished');
-
+          peripheral.disconnect();
         }
       );
     });
